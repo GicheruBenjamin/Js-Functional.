3b2000b91233770a9caf63846816fc86165770f2
GicheruBenjamin <projectbuild201@gmail.com>
2024-05-03 16:45:08 +0300

    Prt6

--- FuncJs/func.js
+++ FuncJs/func.js
@@ -1,10 +1,5 @@
 
 
-// Using the map array function (higher-order function)
-const numbers = [1, 2, 3, 4, 5];
-const doubledNumbers = numbers.map(num => num * 2);
-console.log(doubledNumbers); 
-
 // Example: Pure function
 function add(a, b) {
     return a + b;
--- FuncJs/hof.js
+++ FuncJs/hof.js
@@ -11,3 +11,8 @@ let sum = (a,b) =>{
 }
 
 display(sum(5,7))
+
+let san =[12,53,48,66,11,27,83,79,21];
+console.log(san.map(s => s - 9));
+console.log(san.filter(a => a/2 == 0));
+console.log(san.reduce(s => (s/2) == 0));
